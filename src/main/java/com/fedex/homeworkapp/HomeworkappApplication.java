2b9481049093e965fa0b33f56c2bf19a025051b0
Lili <liliszendrei@gmail.com>
2019-03-06 12:11:06 +0100

    create project

---
+++ src/main/java/com/fedex/homeworkapp/HomeworkappApplication.java
@@ -0,0 +1,13 @@
+package com.fedex.homeworkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkappApplication.class, args);
+    }
+
+}
