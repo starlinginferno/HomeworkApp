875356dce19aada681239c072396e51565f2bfd0
Lili <liliszendrei@gmail.com>
2019-03-06 13:29:03 +0100

    create classes

---
+++ src/main/java/com/fedex/homeworkapp/post/PostService.java
@@ -0,0 +1,10 @@
+package com.fedex.homeworkapp.post;
+
+public interface PostService {
+
+    PostModel createPost();
+
+    void deletePost();
+
+    void editPost();
+}
---
+++ src/main/java/com/fedex/homeworkapp/post/PostServiceImp.java
@@ -0,0 +1,4 @@
+package com.fedex.homeworkapp.post;
+
+public class PostServiceImp {
+}
