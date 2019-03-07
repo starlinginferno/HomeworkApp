702108b814ed39b9a205af60aae68590fffa9c9f
Lili <liliszendrei@gmail.com>
2019-03-07 10:20:17 +0100

    connections done CAREFUL UPDATE

--- src/main/java/com/fedex/homeworkapp/post/PostModel.java
+++ src/main/java/com/fedex/homeworkapp/post/PostModel.java
@@ -1,6 +1,9 @@
 package com.fedex.homeworkapp.post;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fedex.homeworkapp.comment.CommentModel;
+import com.fedex.homeworkapp.user.persistence.model.ApplicationUser;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +11,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Table(name = "posts")
 @Getter
 @Setter
 public class PostModel {
@@ -20,7 +24,12 @@ public class PostModel {
     private String content;
     private Integer score;
 
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private ApplicationUser postsUser;
+
     @JsonManagedReference
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<PostModel> postsComments;
+    @OneToMany(mappedBy = "commentsPost", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CommentModel> postsComments;
 }
